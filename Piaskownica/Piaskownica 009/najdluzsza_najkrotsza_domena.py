9e4e506938a648ec3deab3a1680818f9da6d0d16
quenrythane <artur.babinski.g@gmail.com>
2022-02-27 17:07:09 +0100

    poprawienei czytelnośći

--- Piaskownica/Piaskownica 009/najdluzsza_najkrotsza_domena.py
+++ Piaskownica/Piaskownica 009/najdluzsza_najkrotsza_domena.py
@@ -1,8 +1,7 @@
 # zad 4. Jaka jest najdłuższa i najkrótsza domena w liście flag
-def najdluzsza_najkrotsza_domena(domeny: list) -> str:
-    sorted_by_len_asc = sorted(domeny, key=lambda x: len(x))[1:]
-    sorted_by_len_desc = sorted(domeny, key=lambda x: len(x), reverse=True)
-    shortest = [flaga for flaga in sorted_by_len_asc if len(flaga) == len(sorted_by_len_asc[0])]
-    longest = [flaga for flaga in sorted_by_len_desc if len(flaga) == len(sorted_by_len_desc[0])]
-    return f'najkrótsze({len(shortest[0])} znaków): {shortest}\n' \
-           f'najdłuższe({len(longest[0])} znaków): {longest}'
+def najdluzsza_najkrotsza_domena(domeny: list) -> tuple:
+    sort_domains = sorted(domeny, key=len)
+    shortest = [flaga for flaga in sort_domains if len(flaga) == len(sort_domains[0])]
+    longest = [flaga for flaga in sort_domains[::-1] if len(flaga) == len(sort_domains[::-1][0])]
+    # return sort_domains[0], sort_domains[-1]  <- zwraca 1 najdłuższą i 1 najkrótszą
+    return shortest, longest  # zwraca wszystkie najkrótsze jeśli jest więcej niż 1 i tak samo z najdłuższą
--- Piaskownica/Piaskownica 009/piaskownica 009.py
+++ Piaskownica/Piaskownica 009/piaskownica 009.py
@@ -12,7 +12,7 @@ flagi_oczekujace = flagi_raw[17264:]  # to co na stronie jest od Flagi oczekują
 wszystkie_flagi = flagi + flagi_oczekujace
 
 # flagi zamienione na listy
-flagi_lista = [flaga[5:] for flaga in flagi.split('</p>')]
+flagi_lista = [flaga[5:] for flaga in flagi.split('</p>')][:-1]
 flagi_oczekujace_lista = [flaga[3:] for flaga in flagi_oczekujace.split('</p>')][:-1]
 wszystkie_flagi_lista = flagi_lista + flagi_oczekujace_lista
 
@@ -20,4 +20,9 @@ wszystkie_flagi_lista = flagi_lista + flagi_oczekujace_lista
 print('ilość flag.pl:', policz_flagi_pl.policz_flagi_pl(wszystkie_flagi_lista))
 print('ilość flag.com:', policz_flagi_samo_com.policz_flagi_samo_com(wszystkie_flagi_lista))
 print('ilość liter a w nazwach flag:', policz_literki_a.policz_literki_a(wszystkie_flagi_lista))
-print(najdluzsza_najkrotsza_domena.najdluzsza_najkrotsza_domena(wszystkie_flagi_lista))
+print('najkrótsze domeny:', najdluzsza_najkrotsza_domena.najdluzsza_najkrotsza_domena(wszystkie_flagi_lista)[0])
+print('najdłuższe domeny:', najdluzsza_najkrotsza_domena.najdluzsza_najkrotsza_domena(wszystkie_flagi_lista)[1])
+
+
+
+
